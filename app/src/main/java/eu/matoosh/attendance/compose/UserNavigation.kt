ba85d268ff40919ca3b1b69c1782a4385239ab89
Matus Vanco <matus.vanco@24i.com>
2023-09-28 20:49:05 +0200

    Fix 'QR scanner' navigation item

--- app/src/main/java/eu/matoosh/attendance/compose/UserNavigation.kt
+++ app/src/main/java/eu/matoosh/attendance/compose/UserNavigation.kt
@@ -3,7 +3,6 @@ package eu.matoosh.attendance.compose
 import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.AddCircle
 import androidx.compose.material.icons.filled.List
 import androidx.compose.material.icons.filled.Person
 import androidx.compose.ui.graphics.vector.ImageVector
@@ -15,9 +14,14 @@ object UserRoute {
     const val SCANNER = "Scanner"
 }
 
+sealed interface Icon {
+    data class IconImageVector(val vector: ImageVector?) : Icon
+    data class IconVectorAsset(@DrawableRes val iconRes: Int) : Icon
+}
+
 data class UserDestination(
     val route: String,
-    val icon: ImageVector? = null,
+    val icon: Icon? = null,
     @DrawableRes val iconRes: Int? = null,
     @StringRes val titleId: Int
 )
@@ -25,17 +29,17 @@ data class UserDestination(
 val USER_DESTINATIONS = listOf(
     UserDestination(
         route = UserRoute.PROFILE,
-        icon = Icons.Default.Person,
+        icon = Icon.IconImageVector(Icons.Default.Person),
         titleId = R.string.label_profile
     ),
     UserDestination(
         route = UserRoute.TERMS,
-        icon = Icons.Default.List,
+        icon = Icon.IconImageVector(Icons.Default.List),
         titleId = R.string.label_terms
     ),
     UserDestination(
         route = UserRoute.SCANNER,
-        icon = Icons.Default.AddCircle,
+        icon = Icon.IconVectorAsset(R.drawable.ic_photo),
         titleId = R.string.label_scanner
     )
 )
