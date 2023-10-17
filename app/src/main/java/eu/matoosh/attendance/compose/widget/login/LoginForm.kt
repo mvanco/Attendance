ba8b4b2eac3e980477fd691edf47d1a7c0b28f9f
Matus Vanco <matus.vanco@24i.com>
2023-10-17 22:34:15 +0200

    Change logo

--- app/src/main/java/eu/matoosh/attendance/compose/widget/login/LoginForm.kt
+++ app/src/main/java/eu/matoosh/attendance/compose/widget/login/LoginForm.kt
@@ -1,5 +1,6 @@
 package eu.matoosh.attendance.compose.widget.login
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -8,14 +9,11 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
-import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
@@ -24,7 +22,7 @@ import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -51,33 +49,21 @@ fun LoginForm(
 ) {
     ConstraintLayout(
         modifier = Modifier.fillMaxSize()
+            .background(colorResource(id = R.color.background_overlay_login))
     ) {
         val (form, title) = createRefs()
         createVerticalChain(title, form, chainStyle = ChainStyle.Packed)
-        Row(
+        Image(
             modifier = Modifier
                 .constrainAs(title) {
                     start.linkTo(parent.start)
                     end.linkTo(parent.end)
                     top.linkTo(parent.top)
                     bottom.linkTo(form.top)
-                }
-                .clip(MaterialTheme.shapes.medium)
-                .background(MaterialTheme.colorScheme.surface.copy(alpha = 0.6f))
-                .padding(32.dp)
-
-        ) {
-            Icon(
-                painter = painterResource(id = R.drawable.ic_skater),
-                contentDescription = stringResource(id = R.string.content_description_background_image),
-                tint = MaterialTheme.colorScheme.onSurface
-            )
-            Spacer(Modifier.size(16.dp))
-            Text(
-                text = stringResource(R.string.app_name),
-                style = MaterialTheme.typography.displayMedium
-            )
-        }
+                },
+            painter = painterResource(id = R.drawable.ic_logo),
+            contentDescription = stringResource(id = R.string.content_description_background_image)
+        )
         Column(
             modifier = Modifier
                 .widthIn(max = dimensionResource(id = R.dimen.max_screen_width))
---
+++ app/src/test/java/eu/matoosh/attendance/viewmodels/LoginViewModelUnitTest.kt
@@ -0,0 +1,2 @@
+package eu.matoosh.attendance.viewmodels
+
