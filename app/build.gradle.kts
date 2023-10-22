6ba89af4aee3f36d0f459ae7e1619b7f75a675b7
Matus Vanco <matus.vanco@24i.com>
2023-10-22 19:57:20 +0200

    Release Attendance 1.0.13

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "eu.matoosh.attendance"
         minSdk = 26
         targetSdk = 33
-        versionCode = 10012
-        versionName = "1.0.12"
+        versionCode = 10013
+        versionName = "1.0.13"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
